d6180aa67a32d73f9f3eb257f4f79a436064907c
Brian <bdownes001@gmail.com>
2020-11-21 22:54:06 -0500

    Rock, Paper, Scissor v1 Simple Rock, Paper, Scissor game. Added the games looping and logic to users input. v2 Moved some code into functions. Help Menu, Game Menu, Game Over. Added a score tracker. v3 Able to keep track of players choices. Game will not save any user inputs that are not recognized as being a 'weapon'. Cleaned up code. Changed list to dictionary

--- RPS.py
+++ RPS.py
@@ -3,13 +3,17 @@
 # December 13th 2020
 # https://github.com/bdownes001/cmp135final
 import random
-from collections import Counter
 
-users_favorite = []
+
+player_weapons = {'r': 0, 'p': 0, 's': 0}
 player_win = 0
 ai_win = 0
 tie_win = 0
 weapon_type = ["r", "p", "s"]
+
+
+def decision_making():
+    pass
 
 
 def game_instructions():
@@ -22,22 +26,21 @@ def game_instructions():
 
 def game_menu():
     print("-----------------------------------\n\t\t\t\tMenu\n-----------------------------------")
-    print("Rock, Paper, Scissor.")
+    print("Welcome to Rock, Paper, Scissor.")
     print("Enter 'help' for game instructions.")
     print("Enter 'q' to exit game.")
     print("-----------------------------------\n")
 
 
 def game_over():
-    used_weapons = Counter(users_favorite)
     print("GAME OVER!\nThanks For Playing!")
     print("==============================")
     print(f"Player wins: {player_win}")
     print(f"Computer wins: {ai_win}")
     print(f"Tie games: {tie_win}")
     print(f"Weapons used:")
-    for w in used_weapons:  # Loop though used_weapon list
-        print(f"{w} : {used_weapons[w]}")
+    for k, v in player_weapons.items():
+        print(f"{k}: {v}")
 
 
 game_menu()
@@ -49,41 +52,44 @@ while True:
     print(f"CHEAT: {ai_weapon}")
 
     player_weapon = str(input("Which Weapon do you choose: ")).lower()
-    users_favorite.append(player_weapon)  # Append weapon to user_favorite weapon list
+    # Check users input and store in dictionary
+    if player_weapon == 'r':
+        player_weapons['r'] += 1
+    elif player_weapon == 'p':
+        player_weapons['p'] += 1
+    elif player_weapon == 's':
+        player_weapons['s'] += 1
 
     if player_weapon == 'q':
-        users_favorite.pop()
         game_over()
         break
     elif player_weapon == 'help':
-        users_favorite.pop()
         game_instructions()
     elif player_weapon == ai_weapon:
-        print(f"\nYour choice {player_weapon}\nComputer choice: {ai_weapon}.\nTIE\n")
+        print(f"\nYour choice: {player_weapon}\nComputer choice: {ai_weapon}.\nTIE\n")
         tie_win += 1
     elif player_weapon == 'r':  # Player chooses Rock
         if ai_weapon == 'p':  # AI chooses Paper
-            print(f"\nYour choice {player_weapon}\nComputer choice: {ai_weapon}.\nYou LOSE\n")
+            print(f"\nYour choice: {player_weapon}\nComputer choice: {ai_weapon}.\nYou LOSE\n")
             ai_win += 1
         else:
-            print(f"\nYour choice {player_weapon}\nComputer choice: {ai_weapon}.\nYou WIN\n")
+            print(f"\nYour choice: {player_weapon}\nComputer choice: {ai_weapon}.\nYou WIN\n")
             player_win += 1
     elif player_weapon == 'p':  # Player chooses Paper
         if ai_weapon == 's':  # AI chooses Scissor
-            print(f"\nYour choice {player_weapon}\nComputer choice: {ai_weapon}.\nYou LOSE\n")
+            print(f"\nYour choice: {player_weapon}\nComputer choice: {ai_weapon}.\nYou LOSE\n")
             ai_win += 1
         else:
-            print(f"\nYour choice {player_weapon}\nComputer choice: {ai_weapon}.\nYou WIN\n")
+            print(f"\nYour choice: {player_weapon}\nComputer choice: {ai_weapon}.\nYou WIN\n")
             player_win += 1
     elif player_weapon == 's':  # Player chooses Scissor
         if ai_weapon == 'r':  # AI chooses Rock
-            print(f"\nYour choice {player_weapon}\nComputer choice: {ai_weapon}.\nYou LOSE\n")
+            print(f"\nYour choice: {player_weapon}\nComputer choice: {ai_weapon}.\nYou LOSE\n")
             ai_win += 1
         else:
-            print(f"\nYour choice {player_weapon}\nComputer choice: {ai_weapon}.\nYou WIN\n")
+            print(f"\nYour choice: {player_weapon}\nComputer choice: {ai_weapon}.\nYou WIN\n")
             player_win += 1
     else:
         print("Not an option. Try again")
-        users_favorite.pop()
 
 
